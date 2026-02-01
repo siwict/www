84c155e092064931d9c25aa77369ae3ebc69a865
icstem <michiel@icstem.be>
2026-02-01 11:29:43 +0100

    MAJOR update: versie december 2025

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -17,16 +17,16 @@ function updateLogo() {
   if (isMobile) {
     // Mobile: always small logo
     logo.classList.add("scrolled");
-    logo.src = "images/siw2.png";
+    logo.src = "images/siw5.png";
   } else {
     // Desktop: switch between big and small with hysteresis to prevent flickering
     if (scrollY > 80 && !isScrolled) {
       logo.classList.add("scrolled");
-      logo.src = "images/siw2.png";
+      logo.src = "images/siw5.png";
       isScrolled = true;
     } else if (scrollY < 30 && isScrolled) {
       logo.classList.remove("scrolled");
-      logo.src = "images/siw1.png";
+      logo.src = "images/siw3.png";
       isScrolled = false;
     }
   }
